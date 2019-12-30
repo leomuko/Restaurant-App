6a5185cdefe29a756e1cc10eef0201985d94cb10
leomuko <kezzieleo@gmail.com>
2019-12-30 21:24:02 +0300

    Modify home screen

--- src/screens/homeScreen.js
+++ src/screens/homeScreen.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import SearcBar from '../components/searchBar';
 
 
 const HomeScreen = () =>{
     return(
-        <View>
-            <Text>Hello there</Text>
+        <View >
+            <SearcBar/>
         </View>
     )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    
+});
 
 export default HomeScreen;
