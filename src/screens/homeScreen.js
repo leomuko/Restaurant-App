32d2c0a96c9155dc75543327616f2ab11ef9d8bf
leomuko <kezzieleo@gmail.com>
2019-12-30 20:36:27 +0300

    Create Home Screen

---
+++ src/screens/homeScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+const HomeScreen = () =>{
+    return(
+        <View>
+            <Text>Hello there</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({});
+
+export default HomeScreen;
