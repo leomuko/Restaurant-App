6e48345efec29477a3c00a51b2356949bfb1c6fe
leomuko <kezzieleo@gmail.com>
2020-01-04 14:07:11 +0300

    Create Results Screen

---
+++ src/screens/resultsScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+
+const ResultScreen = () =>{
+ return(
+     <View>
+         <Text>Result Screen</Text>
+     </View>
+ )
+}
+
+const styles = StyleSheet.create({});
+
+export default ResultScreen;
